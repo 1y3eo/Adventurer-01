892ba7dafbee47902a694d30ec0d449710e6bb7e
1y3eo <hyeo131@naver.com>
2023-10-30 23:23:03 +0900

    player move/animation

--- Assets/Game/Script/Character.cs
+++ Assets/Game/Script/Character.cs
@@ -1,16 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    void Start()
+    private CharacterController _cc;
+    public float MoveSpeed = 5f;
+    private Vector3 _movementVelocity;
+    private PlayerInput _playerInput;
+    private float _verticalVelocity;
+    public float Gravity = -9.8f;
+    private Animator _animator;
+
+    private void Awake()
     {
-        
+        _cc = GetComponent<CharacterController>();
+        _playerInput = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void CalculatePlayerMovement()
     {
-        
+        _movementVelocity.Set(_playerInput.HorizontalInput, 0f, _playerInput.VerticalInput);
+        _movementVelocity.Normalize();
+        _movementVelocity = Quaternion.Euler(0, -45f, 0) * _movementVelocity;
+
+        _animator.SetFloat("Speed", _movementVelocity.magnitude);
+
+        _movementVelocity *= MoveSpeed * Time.deltaTime;
+
+        if (_movementVelocity != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(_movementVelocity);
+
+        _animator.SetBool("AirBorne", !_cc.isGrounded);
+    }
+
+    private void FixedUpdate()
+    {
+        CalculatePlayerMovement();
+
+        if (_cc.isGrounded == false)
+            _verticalVelocity = Gravity;
+        else
+            _verticalVelocity = Gravity * 0.3f;
+
+        _movementVelocity += _verticalVelocity * Vector3.up * Time.deltaTime;
+
+        _cc.Move(_movementVelocity);
     }
 }
--- Assets/Game/Script/PlayerInput.cs
+++ Assets/Game/Script/PlayerInput.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public float HorizontalInput;
+    public float VerticalInput;
 
     void Update()
     {
-        
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        VerticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnDisable()
+    {
+        HorizontalInput = 0;
+        VerticalInput = 0;
     }
 }
